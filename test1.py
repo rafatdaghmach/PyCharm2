c3a09f78f810c9f0c0a79ca58d134ec0816fa6d4
rafatdaghmach <32772531+rafatdaghmach@users.noreply.github.com>
2018-01-09 09:15:09 +0100

    delete and write new code

--- test1.py
+++ test1.py
@@ -1,10 +1,3 @@
-class Employee:
-    pass
-
-john = Employee()  # Create an empty employee record
-
-# Fill the fields of the record
-john.name = 'John Doe'
-john.dept = 'computer lab'
-john.salary = 1000
-print(Employee)
+x = 3
+y = 6
+print(x * y)
